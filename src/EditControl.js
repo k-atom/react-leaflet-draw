bfc6264fb7a7f911d4fd7bc111440ac157d58d22
Andrew Dodd <andrew@adimian.com>
2019-01-05 14:34:02 +0100

    Add variant of EditControl that does not auto-create items
    These changes:
     - Use the L.Draw.Event.* defined event names, and include every event
       that the leaflet-draw library defines (because who know which ones
       users want to listen to)
     - Modifies the onCreate behaviour to just shim out to the passed
       onCreated prop (in fact, it just shims out all events this way)
     - Leaves the naming of createDrawControl, as I think this is more
       appropriate to this component

--- src/EditControl.js
+++ src/EditControl.js
@@ -1,139 +1,149 @@
-import { PropTypes } from 'prop-types';
-import Draw from 'leaflet-draw'; // eslint-disable-line
-import isEqual from 'lodash-es/isEqual';
+import { PropTypes } from 'prop-types'
+import Draw from 'leaflet-draw' // eslint-disable-line
+import { isEqual } from 'lodash'
 
-import { MapControl, withLeaflet } from 'react-leaflet';
-import leaflet, { Map, Control } from 'leaflet';
+import { withLeaflet, MapControl } from 'react-leaflet'
+import L, { Map } from 'leaflet'
+
+// import 'leaflet-draw/dist/leaflet.draw.css'
 
 const eventHandlers = {
-  onEdited: 'draw:edited',
-  onDrawStart: 'draw:drawstart',
-  onDrawStop: 'draw:drawstop',
-  onDrawVertex: 'draw:drawvertex',
-  onEditStart: 'draw:editstart',
-  onEditMove: 'draw:editmove',
-  onEditResize: 'draw:editresize',
-  onEditVertex: 'draw:editvertex',
-  onEditStop: 'draw:editstop',
-  onDeleted: 'draw:deleted',
-  onDeleteStart: 'draw:deletestart',
-  onDeleteStop: 'draw:deletestop',
-};
+  onEdited: L.Draw.Event.EDITED,
+  onCreated: L.Draw.Event.CREATED,
+  onDeleted: L.Draw.Event.DELETED,
+
+  onDrawStart: L.Draw.Event.DRAWSTART,
+  onDrawStop: L.Draw.Event.DRAWSTOP,
+  onDrawVertex: L.Draw.Event.DRAWVERTEX,
+
+  onEditStart: L.Draw.Event.EDITSTART,
+  onEditMove: L.Draw.Event.EDITMOVE,
+  onEditResize: L.Draw.Event.EDITRESIZE,
+  onEditVertex: L.Draw.Event.EDITVERTEX,
+  onEditStop: L.Draw.Event.EDITSTOP,
+
+  onDeleteStart: L.Draw.Event.DELETESTART,
+  onDeleteStop: L.Draw.Event.DELETESTOP,
+
+  onToolbarOpened: L.Draw.Event.TOOLBAROPENED,
+  onToolbarCloser: L.Draw.Event.TOOLBARCLOSED,
+  onMarkerContext: L.Draw.Event.MARKERCONTEXT
+}
+
+export const DrawPropTypes = PropTypes.shape({
+  polyline: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  polygon: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  rectangle: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  circle: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  marker: PropTypes.oneOfType([PropTypes.object, PropTypes.bool])
+})
+export const EditPropTypes = PropTypes.shape({
+  edit: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  remove: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  poly: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  allowIntersection: PropTypes.bool
+})
+export const PositionPropType = PropTypes.oneOf([
+  'topright',
+  'topleft',
+  'bottomright',
+  'bottomleft'
+])
 
 class EditControl extends MapControl {
   static propTypes = {
     ...Object.keys(eventHandlers).reduce((acc, val) => {
-      acc[val] = PropTypes.func;
-      return acc;
+      acc[val] = PropTypes.func
+      return acc
     }, {}),
-    onCreated: PropTypes.func,
     onMounted: PropTypes.func,
-    draw: PropTypes.shape({
-      polyline: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-      polygon: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-      rectangle: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-      circle: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-      marker: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-    }),
-    edit: PropTypes.shape({
-      edit: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-      remove: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-      poly: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-      allowIntersection: PropTypes.bool,
-    }),
-    position: PropTypes.oneOf([
-      'topright',
-      'topleft',
-      'bottomright',
-      'bottomleft'
-    ]),
-    leaflet: PropTypes.shape({
-      map: PropTypes.instanceOf(Map),
-      layerContainer: PropTypes.shape({
-        addLayer: PropTypes.func.isRequired,
-        removeLayer: PropTypes.func.isRequired
-      })
+    onWillUnmount: PropTypes.func,
+    draw: DrawPropTypes,
+    edit: EditPropTypes,
+    position: PositionPropType
+  }
+
+  static contextTypes = {
+    map: PropTypes.instanceOf(Map),
+    layerContainer: PropTypes.shape({
+      addLayer: PropTypes.func.isRequired,
+      removeLayer: PropTypes.func.isRequired
     })
-  };
-
-  createLeafletElement(props) {
-    return createDrawElement(props);
   }
 
-  onDrawCreate = (e) => {
-    const { onCreated } = this.props;
-    const { layerContainer } = this.props.leaflet;
-
-    layerContainer.addLayer(e.layer);
-    onCreated && onCreated(e);
-  };
-
-  componentDidMount() {
-    super.componentDidMount();
-    const { map } = this.props.leaflet;
-    const { onMounted } = this.props;
-
+  componentWillMount () {
+    const { map } = this.props.leaflet
+    // register all event handlers for leaflet-draw
     for (const key in eventHandlers) {
       if (this.props[key]) {
-        map.on(eventHandlers[key], this.props[key]);
-      }
-    }
-
-    map.on(leaflet.Draw.Event.CREATED, this.onDrawCreate);
-
-    onMounted && onMounted(this.leafletElement);
-  }
-
-  componentWillUnmount() {
-    super.componentWillUnmount();
-    const { map } = this.props.leaflet;
-
-    map.off(leaflet.Draw.Event.CREATED, this.onDrawCreate);
-
-    for (const key in eventHandlers) {
-      if (this.props[key]) {
-        map.off(eventHandlers[key], this.props[key]);
+        map.on(eventHandlers[key], this.props[key], this)
       }
     }
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidMount () {
+    const { onMounted } = this.props
+    super.componentDidMount()
+    onMounted && onMounted(this.leafletElement)
+  }
+
+  componentWillUnmount () {
+    const { map } = this.props.leaflet
+    const { onWillUnmount } = this.props
+    onWillUnmount && onWillUnmount(this.leafletElement)
+    this.leafletElement.remove(map)
+
+    for (const key in eventHandlers) {
+      if (this.props[key]) {
+        map.off(eventHandlers[key], this.props[key], this)
+      }
+    }
+  }
+
+  createLeafletElement () {
+    return createDrawControl(this.props)
+  }
+
+  componentDidUpdate (prevProps) {
     // super updates positions if thats all that changed so call this first
-    super.componentDidUpdate(prevProps);
+    super.componentDidUpdate(prevProps)
 
-    if (isEqual(this.props.draw, prevProps.draw) || this.props.position !== prevProps.position) {
-      return false;
+    if (
+      isEqual(this.props.draw, prevProps.draw) ||
+      isEqual(this.props.edit, prevProps.edit) ||
+      this.props.position !== prevProps.position
+    ) {
+      return
     }
 
-    const { map } = this.props.leaflet;
+    const { map } = this.props.leaflet
 
-    this.leafletElement.remove(map);
-    this.leafletElement = createDrawElement(this.props);
-    this.leafletElement.addTo(map);
-
-    return null;
+    this.leafletElement.remove(map)
+    this.leafletElement = createDrawControl(this.props)
+    this.leafletElement.addTo(map)
   }
 }
 
-function createDrawElement(props) {
-  const { layerContainer } = props.leaflet;
-  const { draw, edit, position } = props;
+function createDrawControl (props) {
+  const { layerContainer } = props.leaflet
+  const { draw, edit, position } = props
   const options = {
     edit: {
       ...edit,
       featureGroup: layerContainer
     }
-  };
+  }
 
   if (draw) {
-    options.draw = { ...draw };
+    // leaflet-draw mutates underlying object, so create a copy
+    options.draw = { ...draw }
   }
 
   if (position) {
-    options.position = position;
+    options.position = position
   }
 
-  return new Control.Draw(options);
+  return new L.Control.Draw(options) // eslint-disable-line
 }
 
-export default withLeaflet(EditControl);
+export default withLeaflet(EditControl)
