e08fdd011122d2b52b63fe805f51eaba3102adf6
Andrew Dodd <andrew@adimian.com>
2019-01-05 14:40:33 +0100

    Overhaul the example to better work with new code
    NB: The 'Example' class is long, because these changes force all state
    management to be external to the EditControl. I found this works better
    in most cases, as often something else (context, redux store, DOM
    element, whatever) is the main record for these shapes and changes.

--- example/index.js
+++ example/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
-import EditControlExample from "./edit-control";
+import React from 'react'
+import { render } from 'react-dom'
+import EditControlExample from './edit-control'
 
 const example = (
   <div>
     <h1>React-Leaflet-Draw example</h1>
-    <EditControlExample onChange={onChange}/>
+    <p>
+      I do not add polylines. I do not edit rectangles. I do not delete markers.
+    </p>
+    <EditControlExample />
   </div>
-);
+)
 
-function onChange(geojson) {
-  console.log("geojson changed", geojson);
-}
-
-render(example, document.getElementById('app'));
+render(example, document.getElementById('app'))
